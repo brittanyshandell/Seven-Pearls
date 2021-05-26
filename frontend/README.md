7060d3f31e111318a6f61236c98373e2cc160ee8
brittanyshandell <55296933+brittanyshandell@users.noreply.github.com>
2021-05-25 21:22:25 -0500

    first commit

---
+++ frontend/README.md
@@ -0,0 +1,10 @@
+SEVEN PEARLS 
+
+1. Crear folder de estructura
+2. Agregar folder frontend y backend
+3. Crear folder SRC dentro de frontend
+4. Crear el INDEX.html 
+5. Correr nmp init en el folder de front end
+6. nmp install live server
+7. Agregar el comando start en live-server src --verbose
+8. Correr npm start
---
+++ frontend/src/data.js
@@ -0,0 +1,76 @@
+export default{
+    productos: [
+        {
+            _id: '1',
+            name: 'Pulsera Negra Seven Pearls',
+            category: 'Pulseras negras',
+            image:'/images/pulsera (1).jpeg',
+            price: 60,
+            brand: 'Seven Pearls',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+
+        },  
+        {
+            _id: '2',
+            name: 'Pulsera Negra Seven Pearls',
+            category: 'Pulseras negras',
+            image:'/images/pulsera (2).jpeg',
+            price: 60,
+            brand: 'Seven Pearls',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+
+        },  
+        {
+            _id: '3',
+            name: 'Pulsera Negra Seven Pearls',
+            category: 'Pulseras negras',
+            image:'/images/pulsera (3).jpeg',
+            price: 60,
+            brand: 'Seven Pearls',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+
+        },  
+        {
+            _id: '4',
+            name: 'Pulsera Negra Seven Pearls',
+            category: 'Pulseras negras',
+            image:'/images/pulsera (4).jpeg',
+            price: 60,
+            brand: 'Seven Pearls',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+
+        },  
+        {
+            _id: '5',
+            name: 'Pulsera Negra Seven Pearls',
+            category: 'Pulseras negras',
+            image:'/images/pulsera (5).jpeg',
+            price: 60,
+            brand: 'Seven Pearls',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+
+        },  
+        {
+            _id: '6',
+            name: 'Pulsera Negra Seven Pearls',
+            category: 'Pulseras negras',
+            image:'/images/pulsera (52).jpg',
+            price: 60,
+            brand: 'Seven Pearls',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+
+        },  
+    ],
+};
